ec4c44483139f1c9794f2c001acb00e43c8f9ee7
Roja Gnanavel <rojagnanavel@Rojas-MacBook-Pro.local>
2025-12-27 12:10:51 +0530

    leetcode-336-Increasing Triplet Subsequence

---
+++ ArrayandStrings/336.js
@@ -0,0 +1,36 @@
+// 334. Increasing Triplet Subsequence
+// Given an integer array nums, return true if there exists a triple of indices (i, j, k) such that i < j < k and nums[i] < nums[j] < nums[k]. If no such indices exists, return false.
+// Example 1:
+
+// Input: nums = [1,2,3,4,5]
+// Output: true
+// Explanation: Any triplet where i < j < k is valid.
+// Example 2:
+
+// Input: nums = [5,4,3,2,1]
+// Output: false
+// Explanation: No triplet exists.
+// Example 3:
+
+// Input: nums = [2,1,5,0,4,6]
+// Output: true
+// Explanation: One of the valid triplet is (1, 4, 5), because nums[1] == 1 < nums[4] == 4 < nums[5] == 6.
+
+var increasingTriplet = function (nums) {
+  let first = Infinity;
+  let second = Infinity;
+  for (let num of nums) {
+    if (num <= first) {
+      first = num;
+    } else if (num <= second) {
+      second = num;
+    } else {
+      return true;
+    }
+  }
+  return false;
+};
+
+console.log(increasingTriplet([1, 2, 3, 4, 5])); // true
+console.log(increasingTriplet([5, 4, 3, 2, 1])); // false
+console.log(increasingTriplet([2, 1, 5, 0, 4, 6])); // true
